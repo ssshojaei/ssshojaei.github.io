b8b18ebb253ee70cb5d937ee700931efdc1ccbf3
Saleh <ordinarysaleh@pm.me>
2021-02-27 00:47:00 +0330

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-65d0548e5b08833e9d86.js"
+    "url": "webpack-runtime-b87b6a507f937d676ba0.js"
   },
   {
     "url": "framework-55f7b2ba736e89fdc740.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "244d316996de86a4fd19abfe293bebe6"
+    "revision": "36f13d215b648f23eb52b5ba6566e21a"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-a95b272f8c25b9a8a758.js"
